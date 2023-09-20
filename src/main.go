65524cf88f2e15efb01a1d5846b59eff323cc7aa
Lucas <lucas.savy@ynov.com>
2023-09-20 21:08:27 +0200

    Buttons

--- src/main.go
+++ src/main.go
@@ -5,6 +5,8 @@ import (
 	ge "game/gameEngine"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
+	
+	//rlplus "github.com/lachee/raylib-goplus/raylib"
 )
 
 
@@ -15,13 +17,22 @@ func main() {
 	x:= int32(rl.GetMonitorWidth(rl.GetCurrentMonitor()))
 	y := int32(rl.GetMonitorHeight(rl.GetCurrentMonitor()))
 	fmt.Print(x,y)
+	startButton := rl.LoadTexture("assets/Tilesets/bouton_start2.png")
 	quitButton := rl.LoadTexture("assets/Tilesets/bouton_quit2.png")
+	settingsButton := rl.LoadTexture("assets/Tilesets/bouton_settings2.png")
+
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.White)
 		rl.DrawText("aaaaaaaaaa", 10, 10, 20, rl.Black)
-		rl.DrawTexture(quitButton, x/2-x/7, y/3, rl.RayWhite)
+		rl.DrawTexture(startButton, x/2-x/5, y/8, rl.RayWhite)
+		rl.DrawTexture(settingsButton, x/2-x/5, y/4, rl.RayWhite)
+		rl.DrawTexture(quitButton, x/2-x/5, y/2 - 120, rl.RayWhite)
+		
+
+
 		rl.EndDrawing()
 	}
+	
 	rl.CloseWindow()
 }
