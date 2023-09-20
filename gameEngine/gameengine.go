3544da6a1431888f3b46bc9dce1d702bf39028cf
Lucas <lucas.savy@ynov.com>
2023-09-20 14:02:33 +0200

    aaaa

---
+++ gameEngine/gameengine.go
@@ -0,0 +1,48 @@
+package gameEngine
+
+import "fmt"
+
+type GameEngine struct {
+	ScreenWidth int32
+	ScreenHeight int32
+	Running bool
+}
+
+func (g *GameEngine) PrintScreenSize() {
+	fmt.Println(g.ScreenWidth, "*", g.ScreenHeight)
+}
+
+func (g *GameEngine) InitGameEngine(x, y int32) {
+	g.ScreenWidth = x
+	g.ScreenHeight = y
+}
+
+
+//--------------------------------------------
+
+
+type ClassPerso int
+
+const (
+	Archer ClassPerso = iota
+	Warrior ClassPerso = iota
+)
+
+type Personnage struct {
+	name               string
+	class              ClassPerso
+	level              int
+	maxHealthPoint     int
+	currentHealthPoint int
+	inventory          map[string]int
+}
+
+
+func (p *Personnage) Init(Name string, Class ClassPerso, Level int, MaxHealthPoint int, CurrentHealthPoint int, Inventory map[string]int) {
+	p.name = Name
+	p.class = Class
+	p.level = Level
+	p.maxHealthPoint = MaxHealthPoint
+	p.currentHealthPoint = CurrentHealthPoint
+	p.inventory = Inventory
+}
--- main.go
+++ main.go
@@ -1,136 +1,51 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	"main/gameEngine"
 
-const (
-	screenWidth  = 1000
-	screenHeight = 480
-)
-
-var (
-	running  = true
-	bkgColor = rl.NewColor(147, 211, 196, 255)
-
-	grassSprite  rl.Texture2D
-	playerSprite rl.Texture2D
-
-	playerSrc                                     rl.Rectangle
-	playerDest                                    rl.Rectangle
-	playerMoving                                  bool
-	playerDir                                     int
-	playerUp, playerDown, playerRight, playerLeft bool
-	playerFrame                                   int
-
-	frameCount int
-
-	playerSpeed float32 = 3
-
-	cam rl.Camera2D
+	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 func drawScene() {
-	rl.DrawTexture(grassSprite, 100, 50, rl.White)
-	rl.DrawTexturePro(playerSprite, playerSrc, playerDest, rl.NewVector2(playerDest.Width, playerDest.Height), 0, rl.White)
+	rl.DrawText("aaa", 10, 10, 10, rl.Black)
 }
 
 func input() {
-	if rl.IsKeyDown(rl.KeyW) || rl.IsKeyDown(rl.KeyUp) {
-		playerMoving = true
-		playerDir = 1
-		playerUp = true
-	}
-	if rl.IsKeyDown(rl.KeyS) || rl.IsKeyDown(rl.KeyDown) {
-		playerMoving = true
-		playerDir = 0
-		playerDown = true
-	}
-	if rl.IsKeyDown(rl.KeyA) || rl.IsKeyDown(rl.KeyLeft) {
-		playerMoving = true
-		playerDir = 2
-		playerLeft = true
-	}
-	if rl.IsKeyDown(rl.KeyD) || rl.IsKeyDown(rl.KeyRight) {
-		playerMoving = true
-		playerDir = 3
-		playerRight = true
+	if rl.IsKeyDown(rl.KeyEscape) {
+		quit()
 	}
 }
+
 func update() {
-	running = !rl.WindowShouldClose()
-
-	playerSrc.X = playerSrc.Width * float32(playerFrame)
-	if playerMoving {
-		if playerUp {
-			playerDest.Y -= playerSpeed
-		}
-		if playerDown {
-			playerDest.Y += playerSpeed
-		}
-		if playerLeft {
-			playerDest.X -= playerSpeed
-		}
-		if playerRight {
-			playerDest.X += playerSpeed
-		}
-		if frameCount%8 == 1 {
-			playerFrame++
-		}
-	} else if frameCount%45 == 1 {
-		playerFrame++
-	}
-
-	frameCount++
-	if playerFrame > 3 {
-		playerFrame = 0
-	}
-	if !playerMoving && playerFrame > 1 {
-		playerFrame = 0
-	}
-
-	playerSrc.X = playerSrc.Width * float32(playerFrame)
-	playerSrc.Y = playerSrc.Height * float32(playerDir)
-
-	cam.Target = rl.NewVector2(float32(playerDest.X-(playerDest.Width/2)), float32(playerDest.Y-(playerDest.Height/2)))
-	playerMoving = false
-	playerUp, playerDown, playerRight, playerLeft = false, false, false, false
+	var g gameEngine.GameEngine
+	g.Running = !rl.WindowShouldClose()
 }
+
 func render() {
 	rl.BeginDrawing()
-	rl.ClearBackground(bkgColor)
-	rl.BeginMode2D(cam)
+
+	rl.ClearBackground(rl.White)
 
 	drawScene()
 
-	rl.EndMode2D()
 	rl.EndDrawing()
-}
-
-func init() {
-	rl.InitWindow(screenWidth, screenHeight, "Knight Fighter")
-	rl.SetExitKey(0)
-	rl.SetTargetFPS(60)
-
-	grassSprite = rl.LoadTexture("docs/Tilesets/Grass.png")
-	playerSprite = rl.LoadTexture("docs/Characters/Basic Charakter Spritesheet.png")
-
-	playerSrc = rl.NewRectangle(0, 0, 48, 48)
-	playerDest = rl.NewRectangle(200, 200, 100, 100)
-
-	cam = rl.NewCamera2D(rl.NewVector2(float32(screenWidth/2), float32(screenHeight/2)), rl.NewVector2(float32(playerDest.X-(playerDest.Width/2)), float32(playerDest.Y-(playerDest.Height/2))), 0.0, 1.5)
 }
 
 func quit() {
 	rl.CloseWindow()
-	rl.UnloadTexture(playerSprite)
-	rl.CloseWindow()
 }
 
 func main() {
-	for running {
+	var g gameEngine.GameEngine
+	g.InitGameEngine(1920, 1080)
+	g.Running = true
+	rl.InitWindow(g.ScreenWidth, g.ScreenHeight, "Knight Fighter")
+	rl.SetTargetFPS(60)
+
+	for g.Running {
 		input()
 		update()
 		render()
-
 	}
 	quit()
 }
