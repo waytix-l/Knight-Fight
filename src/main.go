fe15ef503104b6aa72c9d7d88f884e76d80644f9
Lucas <lucas.savy@ynov.com>
2023-09-20 20:05:07 +0200

    aaa

--- src/main.go
+++ src/main.go
@@ -9,13 +9,13 @@ import (
 
 func main() {
 	var g ge.GameEngine
-	g.InitGameEngine(1920, 1080, "Knight Fight")
-	caca := rl.LoadTexture("assets/Tilesets/bouton_start.png")
+	g.InitGameEngine(1400, 800, "Knight Fight")
+	caca2 := rl.LoadTexture("assets/Tilesets/bouton_quit2.png")
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.White)
 		rl.DrawText("aaaaaaaaaa", 10, 10, 20, rl.Black)
-		rl.DrawTexture(caca, 20, 20, rl.RayWhite)
+		rl.DrawTexture(caca2, 100, 100, rl.RayWhite)
 		rl.EndDrawing()
 	}
 	rl.CloseWindow()
