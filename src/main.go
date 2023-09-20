df0ee3afc4f36bc2f0f0ca2718ca69a3e636701f
Lucas <lucas.savy@ynov.com>
2023-09-20 20:13:06 +0200

    aaa

--- src/main.go
+++ src/main.go
@@ -11,12 +11,14 @@ func main() {
 	var g ge.GameEngine
 	g.InitGameEngine(1400, 800, "Knight Fight")
 	rl.ToggleFullscreen()
+	x:= int32(rl.GetScreenWidth())
+	y := int32(rl.GetScreenHeight())
 	quitButton := rl.LoadTexture("assets/Tilesets/bouton_quit2.png")
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.White)
 		rl.DrawText("aaaaaaaaaa", 10, 10, 20, rl.Black)
-		rl.DrawTexture(quitButton, 1000, 100, rl.RayWhite)
+		rl.DrawTexture(quitButton, x/2+x/4, y/3, rl.RayWhite)
 		rl.EndDrawing()
 	}
 	rl.CloseWindow()
