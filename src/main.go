61d9888bb92ca9c2ab8c519cd44ce6e5747665a9
Lucas <lucas.savy@ynov.com>
2023-09-20 20:18:17 +0200

    aaa

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	ge "game/gameEngine"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
@@ -9,16 +10,17 @@ import (
 
 func main() {
 	var g ge.GameEngine
-	g.InitGameEngine(1400, 800, "Knight Fight")
+	g.InitGameEngine(100, 800, "Knight Fight")
 	rl.ToggleFullscreen()
-	x:= int32(rl.GetScreenWidth())
-	y := int32(rl.GetScreenHeight())
+	x:= int32(rl.GetMonitorWidth(1))
+	y := int32(rl.GetMonitorHeight(1))
+	fmt.Print(x,y)
 	quitButton := rl.LoadTexture("assets/Tilesets/bouton_quit2.png")
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.White)
 		rl.DrawText("aaaaaaaaaa", 10, 10, 20, rl.Black)
-		rl.DrawTexture(quitButton, x/2+x/4, y/3, rl.RayWhite)
+		rl.DrawTexture(quitButton, x/2, y/3, rl.RayWhite)
 		rl.EndDrawing()
 	}
 	rl.CloseWindow()
