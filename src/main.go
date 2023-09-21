ef8c5321699075bb17675312a9b8c47c35c19d66
Lucas <lucas.savy@ynov.com>
2023-09-21 08:55:32 +0200

    test fond

--- src/main.go
+++ src/main.go
@@ -5,33 +5,31 @@ import (
 	ge "game/gameEngine"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
-	
 	//rlplus "github.com/lachee/raylib-goplus/raylib"
 )
-
 
 func main() {
 	var g ge.GameEngine
 	g.InitGameEngine(1920, 1080, "Knight Fight")
 	rl.ToggleFullscreen()
-	x:= int32(rl.GetMonitorWidth(rl.GetCurrentMonitor()))
+	x := int32(rl.GetMonitorWidth(rl.GetCurrentMonitor()))
 	y := int32(rl.GetMonitorHeight(rl.GetCurrentMonitor()))
-	fmt.Print(x,y)
+	fmt.Print(x, y)
 	startButton := rl.LoadTexture("assets/Tilesets/bouton_start2.png")
 	quitButton := rl.LoadTexture("assets/Tilesets/bouton_quit2.png")
 	settingsButton := rl.LoadTexture("assets/Tilesets/bouton_settings2.png")
+	fond := rl.LoadTexture("assets/Tilesets/Tittle_Screen_jeu.png")
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.White)
-		rl.DrawTexture(startButton, 1200, y/8 + 150, rl.RayWhite)
-		rl.DrawTexture(settingsButton, 1200, y/4 + 150, rl.RayWhite)
-		rl.DrawTexture(quitButton, 1200, y/2 + 15, rl.RayWhite)
-		
-
+		rl.DrawTexture(fond, 0, 0, rl.RayWhite)
+		rl.DrawTexture(startButton, 1200, y/8+150, rl.RayWhite)
+		rl.DrawTexture(settingsButton, 1200, y/4+150, rl.RayWhite)
+		rl.DrawTexture(quitButton, 1200, y/2+15, rl.RayWhite)
 
 		rl.EndDrawing()
 	}
-	
+
 	rl.CloseWindow()
 }
